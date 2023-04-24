6b7c0be88bb342e525baeee0cab83ca1464f5196
Petr Naydin <hint13@gmail.com>
2023-04-24 22:02:44 +0800

    dev(SiteList): Added check for duplicate sites record in application.yaml config file

--- src/main/java/searchengine/config/Site.java
+++ src/main/java/searchengine/config/Site.java
@@ -3,9 +3,23 @@ package searchengine.config;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Setter
 @Getter
 public class Site {
     private String url;
     private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Site site)) return false;
+        return getUrl().equals(site.getUrl());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUrl());
+    }
 }
--- src/main/java/searchengine/config/SitesList.java
+++ src/main/java/searchengine/config/SitesList.java
@@ -1,16 +1,25 @@
 package searchengine.config;
 
-import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.util.LinkedList;
 import java.util.List;
 
-@Getter
 @Setter
 @Component
 @ConfigurationProperties(prefix = "indexing-settings")
 public class SitesList {
     private List<Site> sites;
+
+    public List<Site> getSites() {
+        List<Site> sitesList = new LinkedList<>();
+        sites.forEach(site -> {
+            if (!sitesList.contains(site)) {
+                sitesList.add(site);
+            }
+        });
+        return sitesList;
+    }
 }
