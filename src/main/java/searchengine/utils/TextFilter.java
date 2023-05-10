ef9ed40fe417912b629096e1094cea3f948e2bdb
Petr Naydin <hint13@gmail.com>
2023-05-10 23:14:06 +0800

    dev(TextFilter): Added calcLemmas for class. need fix: MORPH_PARTS

--- src/main/java/searchengine/utils/TextFilter.java
+++ src/main/java/searchengine/utils/TextFilter.java
@@ -15,11 +15,11 @@ import java.util.regex.Pattern;
 public class TextFilter {
     private static final String[] MORPH_PARTS = {
             "СОЮЗ", "ПРЕДЛ", "ЧАСТ", "CONJ", "PREP", "ARTICLE"
-            //"МЕЖД", "МС", "МС-П", "ВВОДН", "PN", "ADJECTIVE"
+//            "МЕЖД", "МС", "МС-П", "ВВОДН", "PN", "ADJECTIVE"
     };
     private String text;
-    private LuceneMorphology rusLM;
-    private LuceneMorphology engLM;
+    private final LuceneMorphology rusLM;
+    private final LuceneMorphology engLM;
 
     public TextFilter(String text, LuceneMorphology engLuceneMorph, LuceneMorphology rusLuceneMorph) {
         this.text = text;
@@ -50,7 +50,7 @@ public class TextFilter {
     }
 
     private LuceneMorphology getLuceneMorphForWord(String word) {
-        if (word.matches("\\w")) {
+        if (word.matches("\\w+")) {
             return engLM;
         }
         return rusLM;
@@ -60,18 +60,21 @@ public class TextFilter {
         Map<String, Integer> lemmasCount = new HashMap<>();
 
         List<String> words = getWords(text);
-        words.forEach(System.out::println);
-
+        for (String word : words) {
+            List<String> lemmas = getLemmas(word, true);
+            for (String lemma: lemmas) {
+                if (!lemmasCount.containsKey(lemma)) {
+                    lemmasCount.put(lemma, 0);
+                }
+                lemmasCount.put(lemma, lemmasCount.get(lemma) + 1);
+            }
+        }
         return lemmasCount;
-    }
-
-    public Set<String> getLemmasForText(String text) {
-        return getWordsLemmas(getWords(text));
     }
 
     public List<String> getWords(String text) {
         List<String> res = new ArrayList<>();
-        Pattern pattern = Pattern.compile("[^\\S\\w-']", Pattern.UNICODE_CHARACTER_CLASS);
+        Pattern pattern = Pattern.compile("\\s|\\d+-?\\w*|\\W", Pattern.UNICODE_CHARACTER_CLASS);
 
         String[] words = pattern.split(text.toLowerCase());
 
@@ -84,16 +87,12 @@ public class TextFilter {
         return res;
     }
 
-    public Set<String> getWordsLemmas(List<String> words) {
-        Set<String> lemmas = new HashSet<>();
-        for (String word : words) {
-            lemmas.addAll(getLemmas(getLuceneMorphForWord(word), word, true));
-        }
-        return lemmas;
-    }
-
-    public static List<String> getLemmas(LuceneMorphology luceneMorph, String word, boolean checkValid) {
+    public List<String> getLemmas(String word, boolean checkValid) {
         List<String> lemmas = new LinkedList<>();
+        if (word.length() < 2) {
+            return lemmas;
+        }
+        LuceneMorphology luceneMorph = getLuceneMorphForWord(word);
         List<String> wordBaseForms = luceneMorph.getMorphInfo(word);
         for (String wordBaseForm : wordBaseForms) {
             String[] wordFormParts = wordBaseForm.split("\\|");
@@ -105,12 +104,12 @@ public class TextFilter {
         return lemmas;
     }
 
-    private static boolean isNotValidForm(String wordFormPart) {
+    private boolean isNotValidForm(String wordFormPart) {
         boolean isNotValid = false;
         for (String part : MORPH_PARTS) {
             isNotValid = isNotValid || wordFormPart.contains(part);
         }
-        return  isNotValid;
+        return isNotValid;
     }
 
     public static void main(String[] args) throws IOException {
@@ -119,20 +118,14 @@ public class TextFilter {
                 "Даже всемогущая пунктуация не имеет власти над рыбными текстами, ведущими безорфографичный образ жизни. Однажды одна маленькая строчка рыбного текста по имени Lorem ipsum решила выйти в большой мир грамматики. Великий Оксмокс предупреждал ее о злых запятых, диких знаках вопроса и коварных точках с запятой, но текст не дал сбить себя с толку.\n" +
                 "Он собрал семь своих заглавных букв, подпоясал инициал за пояс и пустился в дорогу. Взобравшись на первую вершину курсивных гор, бросил он последний взгляд назад, на силуэт своего родного города Буквоград, на заголовок деревни Алфавит и на подзаголовок своего переулка Строчка. Грустный риторический вопрос скатился по его щеке и он продолжил свой путь. По дороге встретил текст рукопись. Она предупредила его: «В моей стране все переписывается по несколько раз. Единственное, что от меня осталось, это приставка «и». Возвращайся ты лучше в свою безопасную страну». Не послушавшись рукописи, наш текст продолжил свой путь. Вскоре ему повстречался коварный составитель\n";
 
-        String text = "\n" +
-                "West End musicals\n" +
-                "The 'West End is London's theatreland - home to over forty theatres. London's plays, shows, and operas attract around 11 million visitors per year and, with tickets costing around £30 each, they bring a lot of income into the capital. The biggest West End attractions are always musicals. Cats ran for 21 years, and Les Miserables is currently celebrating its 18th year in the West End. Here are some of the hottest tickets in town today. \n" +
+        String text = "West End musicals\n" +
+                "The \"West End\" is London's theatreland - home to over forty theatres. London's plays, shows, and operas attract around 11 million visitors per year and, with tickets costing around £30 each, they bring a lot of income into the capital. The biggest West End attractions are always musicals. Cats ran for 21 years, and Les Miserables is currently celebrating its 18th year in the West End. Here are some of the hottest tickets in town today. \n" +
                 "My Fair Lady is based on G. B. Shaw's 1916 play Pygmalion and tells the story of Eliza Doolittle, the Cockney flower-seller chosen from the streets by a professor of linguistics and transformed into a lady. This current production opened in 2001 starring Martine McCutcheon from the TV soap EastEnders. It features well-known songs such as \"I'm getting married in the morning\" and \"On the street where you live\". \n" +
                 "Bombay Dreams is based in the Indian film industry and features an all-Asian cast. The story centers around Akaash, a poor boy played by Raza Jaffrey, who becomes a film star and falls in love with the daughter of one of Bombay's greatest film directors. The show features modern Indian pop music, such as \"Shakalaka Baby\", and dazzling costumes and choreography. It opened in 2002 and quickly became one of the most popular shows in London.\n" +
                 "We Will Rock You opened in 2002. This musical, with a script by comedian Ben Elton, takes place in the future when rock music is illegal. The story is based on famous songs by Queen such as \"Bohemian Rhapsody\" and \"I Want to Break Free\", the set resembles a rock concert and there are plenty of special effects. And Tony Vincent, who plays the hero Galileo Figaro, sounds very like Queen's lead singer, Freddie Mercury.";
 
-//        LuceneMorphology luceneMorphology = new EnglishLuceneMorphology();
-//        String[] words = {"bombay", "dreams", "is", "based", "in", "the", "indian", "film", "industry", "and", "features", "an", "all-asian", "cast"};
-//        Arrays.stream(words).forEach(w -> {
-//            List<String> forms = luceneMorphology.getMorphInfo(w);
-//            forms.forEach(System.out::println);
-//        });
-        TextFilter textFilter = new TextFilter(text + " " + html);
+//        TextFilter textFilter = new TextFilter(text + " " + html);
+        TextFilter textFilter = new TextFilter("in on at by one two three two three three");
         textFilter.calcLemmas().forEach((key, value) -> System.out.println(key + ": " + value));
     }
 }
