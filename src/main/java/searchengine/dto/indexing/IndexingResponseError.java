bd39f97aa68e922eee12dff6ad11db8ccf27ee50
Petr Naydin <hint13@gmail.com>
2023-05-18 22:07:18 +0800

    feat: added static members for error messages at IndexingResponseError. Added constructor overload with errorMessage parameter

--- src/main/java/searchengine/dto/indexing/IndexingResponseError.java
+++ src/main/java/searchengine/dto/indexing/IndexingResponseError.java
@@ -6,9 +6,19 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class IndexingResponseError extends IndexingResponse {
+    public static final String msgIndexingAlreadyStarted = "Индексация уже запущена";
+    public static final String msgIndexingNotStarted = "Индексация не запущена";
+    public static final String msgBadPageUrl =
+            "Данная страница находится за пределами сайтов, указанных в конфигурационном файле";
+
     private String error;
     public IndexingResponseError(boolean isIndexing) {
         setResult(false);
-        error = isIndexing ? "Индексация уже запущена" : "Индексация не запущена";
+        error = isIndexing ? msgIndexingAlreadyStarted : msgIndexingNotStarted;
+    }
+
+    public IndexingResponseError(String errorMessage) {
+        setResult(false);
+        error = errorMessage;
     }
 }
